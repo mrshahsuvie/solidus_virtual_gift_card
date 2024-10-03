fbc14043948ae0df2e505a708af93bcb56769f7b
mayur <mrshah@suvie.com>
2024-10-03 18:17:47 +0530

    introduced new setting for email

--- app/decorators/models/solidus_virtual_gift_card/spree/order_decorator.rb
+++ app/decorators/models/solidus_virtual_gift_card/spree/order_decorator.rb
@@ -5,7 +5,10 @@ module SolidusVirtualGiftCard
     module OrderDecorator
       def self.prepended(base)
         base.class_eval do
-          state_machine.after_transition to: :complete, do: :send_gift_card_emails
+          # Only add the callback if sending emails is enabled
+          if SolidusVirtualGiftCard.configuration.gift_card_email_notifications_enabled
+            state_machine.after_transition to: :complete, do: :send_gift_card_emails
+          end
 
           has_many :gift_cards, through: :line_items
         end
